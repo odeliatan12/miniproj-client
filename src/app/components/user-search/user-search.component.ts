46ca4f4c5d473030d4c988434ba65501eb8ac501
odeliatan12 <odeliatan1@gmail.com>
2023-04-30 15:25:08 +0800

    finish set up test later

--- src/app/components/user-search/user-search.component.ts
+++ src/app/components/user-search/user-search.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-search',
   templateUrl: './user-search.component.html',
   styleUrls: ['./user-search.component.css']
 })
-export class UserSearchComponent {
+export class UserSearchComponent implements OnInit {
+
+  title: string = 'AGM project';
+  latitude!: number;
+  longitude!: number;
+  zoom!: number;
+  address!: string;
+  private geoCoder: any;
+
+  constructor( private mapsAPILoader: MapsAPILoader, private ngZone: NgZone ) { }
+
+  ngOnInit(): void {
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+    });
+  }
+
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 8;
+        this.getAddress(this.latitude, this.longitude);
+      });
+    }
+  }
+
+  getAddress(latitude: number, longitude: number) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results: { formatted_address: string; }[], status: string) => {
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    
+    });
+  }
 
 }
