504cf4bbdb87fb6e6417f67f0c0a38bb6f3af192
odeliatan12 <odeliatan1@gmail.com>
2023-05-22 12:05:29 +0800

    update angular

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserAuthService } from '../services/user-auth.service';
 import { UserService } from '../services/user.service';
@@ -7,7 +7,7 @@ import { UserService } from '../services/user.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(private authService: UserAuthService, private userService: UserService, private route: Router){}
 
